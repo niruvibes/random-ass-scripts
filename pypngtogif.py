91e23861f838414a11633c666f8b32f53168c009
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2022-12-08 18:45:18 +1100

    file and arguments
    This update allows for you to just state which images to convert in cmd and the fps for it

--- pypngtogif.py
+++ pypngtogif.py
@@ -1,17 +1,25 @@
+import argparse
 import imageio
 
-# Create an array of image filenames
-images = ['image1.png', 'image2.png', 'image3.png']
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser()
+
+# Add arguments for the filenames and fps
+parser.add_argument('filenames', nargs='+', help='list of filenames to include in the gif')
+parser.add_argument('--fps', type=int, default=10, help='frame rate of the gif (frames per second)')
+
+# Parse the command-line arguments
+args = parser.parse_args()
 
 # Create an empty list to hold the images
 image_list = []
 
-# Iterate over the list of images
-for image in images:
+# Iterate over the list of filenames
+for filename in args.filenames:
     # Load each image into memory
-    img = imageio.imread(image)
+    img = imageio.imread(filename)
     # Add the image to the list of images
     image_list.append(img)
 
 # Save the image list as a gif
-imageio.mimwrite('animated.gif', image_list, fps=10)
+imageio.mimwrite('animated.gif', image_list, fps=args.fps)
