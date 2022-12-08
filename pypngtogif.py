c5bbca385d2d4ed4f4ad6f21d5e7a25e58d38c70
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2022-12-08 22:19:33 +1100

    fuck it we rewrite

--- pypngtogif.py
+++ pypngtogif.py
@@ -1,44 +1,18 @@
-import argparse
-import imageio
-import importlib.util
-import subprocess
 from PIL import Image
 
-# Check if the imageio and Pillow libraries are installed
-if importlib.util.find_spec('imageio') is None or importlib.util.find_spec('Pillow') is None:
-    # Install the libraries if they are not installed
-    subprocess.run(['pip', 'install', 'imageio'])
-    subprocess.run(['pip', 'install', 'Pillow'])
+# Open the first image and get the size
+first_image = Image.open('image1.jpg')
+width, height = first_image.size
 
-# Create an ArgumentParser object
-parser = argparse.ArgumentParser()
+# Create an empty image with the same size
+gif_image = Image.new('RGB', (width, height))
 
-# Add arguments for the filenames, fps, and size
-parser.add_argument('filenames', nargs='+', help='list of filenames to include in the gif')
-parser.add_argument('--fps', type=int, default=10, help='frame rate of the gif (frames per second)')
-parser.add_argument('--size', type=int, nargs=2, default=(640, 480), help='width and height of the gif in pixels')
-
-# Parse the command-line arguments
-args = parser.parse_args()
-
-# Create an empty list to hold the images
+# Open all of the images and convert them to RGB mode
 image_list = []
+for i in range(1, 10):
+    image = Image.open(f'image{i}.jpg')
+    image = image.convert('RGB')
+    image_list.append(image)
 
-# Iterate over the list of filenames
-for filename in args.filenames:
-    # Load each image into memory
-    img = Image.open(filename)
-    # Resize the image to the specified size
-    img = img.resize(args.size)
-    # Convert the image to a NumPy array
-    img = imageio.imread(filename)
-    # Add the image to the list of images
-    image_list.append(img)
-
-# Save the image list as a gif
-imageio.save('animated.gif', image_list, fps=args.fps)
-
-
-#usage
-#python [scriptname].py image1.png image2.png image3.png --fps [fps number] --size [sizex] [sizey]
-#python pypngtogif.py image1.png image2.png image3.png --fps 15 --size 320 320
+# Save the images as a GIF
+gif_image.save('animated.gif', save_all=True, append_images=image_list)
