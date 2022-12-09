0645fd62834df1e476549551d449f17435a6a885
niru.vibes <67180137+niruvibes@users.noreply.github.com>
2022-12-09 15:02:00 +1100

    WHATW THE FUCK IS GOING ON

--- imagetogif/pypngtogif.py
+++ imagetogif/pypngtogif.py
@@ -1,6 +1,8 @@
 import argparse
+import os
 import importlib.util
 import imageio
+import imghdr
 import subprocess
 import numpy
 from PIL import Image
@@ -24,19 +26,35 @@ else:
     from PIL import Image
 
 # Create an ArgumentParser object
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 # Add arguments for the filenames, fps, and size
 parser.add_argument('filenames', nargs='+', help='list of filenames to include in the gif')
 parser.add_argument('-n', '--name', default='animated.gif', help='The name of the GIF')
 parser.add_argument('-f', '--fps', type=int, default=10, help='frame rate of the gif (frames per second)')
 parser.add_argument('-s', '--size', type=int, nargs=2, default=(320, 320), help='width and height of the gif in pixels')
+parser.add_argument('-o', '--output-directory', help='The directory where the output GIF should be saved')
+# Add the `--loop` flag
+parser.add_argument('-l', '--loop', type=int, default=0, help='The number of times the GIF should loop, -1 = infinte, 0 = once, positive = number, cannot work with duration')
+# Add the `--duration` flag
+parser.add_argument('-d', '--duration', type=int, default=None, help='The total duration of the output GIF in seconds, cannot work with loop')
+# Add the `--compression` flag
+parser.add_argument('-c', '--compression', type=int, default=10, help='The level of compression to be used for the output GIF, cannot work with target-size')
+# Add the `--target-size` flag
+parser.add_argument('-t', '--target-size', type=int, default=None, help='The target size of the output GIF in bytes, cannot work with compression')
 
 # Parse the command-line arguments
 args = parser.parse_args()
 
 # Create an empty list to hold the images
 image_list = []
+
+# Check the file type of each input image
+for filename in args.filenames:
+    file_type = imghdr.what(filename)
+    if file_type is None:
+        print(f'{filename} is not a valid image file')
+        continue
 
 # Iterate over the list of filenames
 for filename in args.filenames:
@@ -66,5 +84,53 @@ for filename in args.filenames:
         # Print an error message if there was a problem loading the image
         print(f'Failed to load image: {filename}')
 
-# Save the image list as a gif
-imageio.mimwrite(args.name + '.gif', image_list, fps=args.fps)
+# Check if both the --compression and --target-size flags were specified
+if args.compression is not None and args.target_size is not None:
+    print('Error: The --compression and --target-size flags are mutually exclusive')
+    exit()
+
+# Check if the --compression flag was specified
+if args.compression is None:
+    # Calculate the total size of the input images in bytes
+    total_size = 0
+    for img in image_list:
+        total_size += img.nbytes
+
+    # Calculate the ratio of the target size to the total size of the input images
+    size_ratio = args.target_size / total_size
+
+    # Calculate the level of compression to use based on the size ratio
+    compression = int(size_ratio * 100)
+else:
+    # Use the value of the --compression flag as the level of compression
+    compression = args.compression
+
+
+# Check if both the `--loop` and `--duration` flags were specified
+if args.loop is not None and args.duration is not None:
+    print('Error: The --loop and --duration flags cannot be used together')
+    exit()
+
+# Check if the `--loop` or `--duration` flags were specified
+if args.loop is not None or args.duration is not None:
+    # Set the `duration` argument to the duration of the output gif in seconds
+    duration = args.duration
+    if args.loop == -1:
+        # Set the duration to None if the gif should loop indefinitely
+        duration = None
+    else:
+        # Calculate the duration of the gif in seconds
+        duration = len(image_list) / args.fps
+        if args.loop > 0:
+            # Multiply the duration by the number of times the gif should loop
+            duration *= args.loop
+
+# Save the image list as a gif using the calculated or specified level of compression
+imageio.mimwrite(os.path.join(args.output_directory, args.name + '.gif'), image_list, fps=args.fps, loop=args.loop, compression=compression)
+
+
+#usage
+#python create_gif.py img1.jpg img2.png img3.jpeg -n my_gif.gif -f 15 -s 512 512 -o gif_output_dir -l -c 5
+#python pypngtogif.py image1.png image2.png image3.png --name [name of output] --fps [fps of gif] --size [size of output gif] --output-directory [directory to save] -l 3 -c 5
+#note!: the compression and target size flags cannot be used together, the program will exit
+#note!: the duration and  size flags cannot be used together, the program will exit
